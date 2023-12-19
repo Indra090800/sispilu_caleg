3335023976e57c4ca2b5992df3969453875e955f
Indra090800 <indramaulana.9c@gmail.com>
2023-12-19 21:23:02 +0700

    BARU DB

--- sispilu.sql
+++ sispilu.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Waktu pembuatan: 08 Des 2023 pada 04.20
+-- Waktu pembuatan: 19 Des 2023 pada 15.22
 -- Versi server: 10.4.32-MariaDB
 -- Versi PHP: 8.2.12
 
@@ -28,9 +28,21 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE `tb_log` (
-  `id_log` int(11) NOT NULL,
-  `deskripsi` varchar(100) NOT NULL
+  `id_saksi` int(11) NOT NULL,
+  `deskripsi` varchar(100) NOT NULL,
+  `id` int(11) NOT NULL,
+  `id_tps` int(11) NOT NULL,
+  `jam` varchar(10) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data untuk tabel `tb_log`
+--
+
+INSERT INTO `tb_log` (`id_saksi`, `deskripsi`, `id`, `id_tps`, `jam`) VALUES
+(0, '6 delete vote from 6 in 2', 6, 2, '09:54'),
+(6, '6 delete vote 2 for 6 in 2', 6, 2, '09:56'),
+(6, '6 delete vote from 6 in 2', 6, 2, '09:56');
 
 -- --------------------------------------------------------
 
@@ -98,8 +110,10 @@ CREATE TABLE `tb_saksi` (
 
 INSERT INTO `tb_saksi` (`id_saksi`, `nik_ktp`, `nama_saksi`, `id_parpol`, `alamat`, `no_hp`, `foto_saksi`, `id_tps`, `password`) VALUES
 (3, '12345', 'Andri Mulyana', '1', 'Jln. Sempu 1 No.7 Kel. Beji, Kec. Beji Kota Depok', '082118471055', '12123.png', '1', '$2y$12$GH4baR9UVoKtlBbqYxQOb.HxwTR2lQgXBC2.oI2LjqLj/L2lr6Nw.'),
-(5, '123456', 'Randa', '1', 'Jln. Sempu 1 No.7 Kel. Beji, Kec. Beji Kota Depok', '082118471055', '12123.png', '2', '$2y$12$GH4baR9UVoKtlBbqYxQOb.HxwTR2lQgXBC2.oI2LjqLj/L2lr6Nw.'),
-(6, '123457', 'Duda', '1', 'Jln. Sempu 1 No.7 Kel. Beji, Kec. Beji Kota Depok', '082118471055', '12123.png', '3', '$2y$12$GH4baR9UVoKtlBbqYxQOb.HxwTR2lQgXBC2.oI2LjqLj/L2lr6Nw.');
+(5, '123456', 'Randa', '1', 'Jln. Sempu 1 No.7 Kel. Beji, Kec. Beji Kota Depok', '082118471055', '12123.png', '3', '$2y$12$ayuHlkUCXuWF3vqeHpSYxe1dCObke4WqeYCLlDcd8jgARB/rFqIm2'),
+(6, '123457', 'Duda', '1', 'Jln. Sempu 1 No.7 Kel. Beji, Kec. Beji Kota Depok', '082118471055', '12123.png', '2', '$2y$12$aI/cvlcMZ6df9zYxVy6A5OP/wLz64/THV0teuIK3lbyGEwyXBAOJq'),
+(7, '123458', 'Wajib', '1', 'Beji', '90032342909', '123458.png', '4', '$2y$12$aQUxjrxJgrqCCs222I86PODknfqm9mSUqG4tUlwbBVFoPtLeHNhZm'),
+(8, '123459', 'INDRA M', '1', 'BEJI', '979878970', '123459.jpg', '5', '$2y$12$3LSGZGSCyKjzAifYSX/F0eUZMWqvs0LpEZhqkGrxCDYOOHSIrYNU.');
 
 -- --------------------------------------------------------
 
@@ -124,7 +138,36 @@ CREATE TABLE `tb_tps` (
 INSERT INTO `tb_tps` (`id_tps`, `nama_tps`, `alamat`, `desa`, `kecamatan`, `lokasi`, `foto_bukti`) VALUES
 (1, 'TPS 01', 'Grogol RT 03/08, Cilegon Barat', 'Grogol', 'Cilegon Barat', '-6.362709080711586, 106.8237540967312', '1.jpg'),
 (2, 'TPS 02', 'Grogol RT 03/08, Cilegon Barat', 'Grogol', 'Cilegon Barat', '-6.362709080711586, 106.8237540967312', '2.jpg'),
-(3, 'TPS 03', 'Grogol RT 03/08, Cilegon Barat', 'Grogol', 'Cilegon Barat', '-6.362709080711586, 106.8237540967312', '3.jpg');
+(3, 'TPS 03', 'Grogol RT 03/08, Cilegon Barat', 'Grogol', 'Cilegon Barat', '-6.362709080711586, 106.8237540967312', '3.jpg'),
+(4, 'TPS 04', 'Beji', 'Beji', 'Beji', '121231009, -213217390', NULL),
+(5, 'TPS 05', 'Hawai', 'Hawai', 'Beji Timur', '-6.362709080711586, 106.8237540967312', NULL);
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `tb_traffic`
+--
+
+CREATE TABLE `tb_traffic` (
+  `id_traffic` int(11) NOT NULL,
+  `jml_vote` int(11) NOT NULL,
+  `id` int(11) NOT NULL,
+  `jam` varchar(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data untuk tabel `tb_traffic`
+--
+
+INSERT INTO `tb_traffic` (`id_traffic`, `jml_vote`, `id`, `jam`) VALUES
+(1, 66, 6, '15'),
+(2, 5, 6, '15'),
+(3, 9, 6, '15'),
+(4, 21, 6, '15'),
+(5, 6, 6, '09:52'),
+(6, 8, 6, '09:53'),
+(7, 6, 6, '09:55'),
+(8, 6, 6, '09:56');
 
 -- --------------------------------------------------------
 
@@ -172,8 +215,7 @@ CREATE TABLE `tb_vote_caleg` (
 --
 
 INSERT INTO `tb_vote_caleg` (`id_saksi`, `id_tps`, `jml_vote`, `jam`, `id`) VALUES
-(3, 1, 4, '10', 6),
-(5, 2, 64, '10', 6);
+(8, 5, 4, '15:32', 6);
 
 -- --------------------------------------------------------
 
@@ -222,12 +264,6 @@ INSERT INTO `users` (`id`, `nik`, `nama_caleg`, `email`, `password`, `id_role`, 
 --
 
 --
--- Indeks untuk tabel `tb_log`
---
-ALTER TABLE `tb_log`
-  ADD PRIMARY KEY (`id_log`);
-
---
 -- Indeks untuk tabel `tb_parpol`
 --
 ALTER TABLE `tb_parpol`
@@ -252,6 +288,12 @@ ALTER TABLE `tb_tps`
   ADD PRIMARY KEY (`id_tps`);
 
 --
+-- Indeks untuk tabel `tb_traffic`
+--
+ALTER TABLE `tb_traffic`
+  ADD PRIMARY KEY (`id_traffic`);
+
+--
 -- Indeks untuk tabel `tb_voters`
 --
 ALTER TABLE `tb_voters`
@@ -269,12 +311,6 @@ ALTER TABLE `users`
 --
 
 --
--- AUTO_INCREMENT untuk tabel `tb_log`
---
-ALTER TABLE `tb_log`
-  MODIFY `id_log` int(11) NOT NULL AUTO_INCREMENT;
-
---
 -- AUTO_INCREMENT untuk tabel `tb_parpol`
 --
 ALTER TABLE `tb_parpol`
@@ -290,13 +326,19 @@ ALTER TABLE `tb_role`
 -- AUTO_INCREMENT untuk tabel `tb_saksi`
 --
 ALTER TABLE `tb_saksi`
-  MODIFY `id_saksi` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `id_saksi` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 
 --
 -- AUTO_INCREMENT untuk tabel `tb_tps`
 --
 ALTER TABLE `tb_tps`
-  MODIFY `id_tps` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id_tps` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT untuk tabel `tb_traffic`
+--
+ALTER TABLE `tb_traffic`
+  MODIFY `id_traffic` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 
 --
 -- AUTO_INCREMENT untuk tabel `tb_voters`
